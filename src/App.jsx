c884d7f0ba592bd7a365ba6b18b32892ad5ab55f
bwng0v0 <ehs4742@gmail.com>
2024-07-25 15:06:37 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>오늘은</h1>
+      <h1>어떤</h1>
+      <h1>하루인가요?</h1>
+      <a href='#'>link</a>
+      <div className='tags'>
+
+      </div>
+      
+      
+      <button><p>일정 추천받기!</p></button>
+    </div>
+  )
+}
+
+export default App
